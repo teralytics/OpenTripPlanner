0076d7a33c1a7bd7350e01b7474ca66871e13e85
irakitin <ilya.rakitsin@teralytics.ch>
2015-07-30 16:17:08 +0200

    RADAR-105 - extending multi target strategy

--- src/main/java/org/opentripplanner/routing/algorithm/strategies/MultiTargetTerminationStrategy.java
+++ src/main/java/org/opentripplanner/routing/algorithm/strategies/MultiTargetTerminationStrategy.java
@@ -1,12 +1,13 @@
 package org.opentripplanner.routing.algorithm.strategies;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.opentripplanner.routing.core.RoutingRequest;
 import org.opentripplanner.routing.core.State;
 import org.opentripplanner.routing.graph.Vertex;
 import org.opentripplanner.routing.spt.ShortestPathTree;
+import org.opentripplanner.util.DateUtils;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * A termination strategy that terminates after multiple targets have been reached.
@@ -21,9 +22,23 @@ public class MultiTargetTerminationStrategy implements SearchTerminationStrategy
     private final Set<Vertex> unreachedTargets;
     private final Set<Vertex> reachedTargets;
     
+    private double reachPercentage;
+    private long abortTime = Long.MIN_VALUE;
+    private double timeout;
+
     public MultiTargetTerminationStrategy(Set<Vertex> targets) {
+        this(targets, Long.MIN_VALUE);
+    }
+
+    public MultiTargetTerminationStrategy(Set<Vertex> targets, double timeout, double reach) {
         unreachedTargets = new HashSet<Vertex>(targets);
         reachedTargets = new HashSet<Vertex>(targets.size());
+        this.timeout = timeout;
+        this.reachPercentage = reach;
+    }
+
+    public MultiTargetTerminationStrategy(Set<Vertex> targets, double timeout) {
+        this(targets, timeout, 0.95);
     }
 
     public Set<Vertex> getUnreachedTargets() {
@@ -41,6 +56,7 @@ public class MultiTargetTerminationStrategy implements SearchTerminationStrategy
     @Override
     public boolean shouldSearchTerminate(Vertex origin, Vertex target, State current,
                                          ShortestPathTree spt, RoutingRequest traverseOptions) {
+        setAbortTime();
         Vertex currentVertex = current.getVertex();
                 
         // TODO(flamholz): update this to handle vertices that are not in the graph
@@ -49,7 +65,22 @@ public class MultiTargetTerminationStrategy implements SearchTerminationStrategy
             unreachedTargets.remove(currentVertex);
             reachedTargets.add(currentVertex);
         }
-        return unreachedTargets.size() == 0;
+        return unreachedTargets.size() == 0 || 
+            (abortTime < Long.MAX_VALUE && System.currentTimeMillis() > abortTime && getReachedRatio() >= reachPercentage);
+    }
+    
+    private double getReachedRatio() {
+        int nReached = reachedTargets.size();
+        int total = nReached + unreachedTargets.size();
+        return 1.0 * nReached / total;
+    }
+    
+    private void setAbortTime() {
+        if (abortTime == Long.MIN_VALUE && timeout != Double.MIN_VALUE) {
+            abortTime = DateUtils.absoluteTimeout(timeout);
+        } else if (timeout == Double.MIN_VALUE) {
+            abortTime = Long.MAX_VALUE;
+        }
     }
 
 }
