f2e8014006196c7938fe4bdc2c513b673999ef64
irakitin <ilya.rakitsin@teralytics.ch>
2018-05-27 16:24:26 +0200

    PROD-1358 changing tiles

--- src/client/js/otp/config.js
+++ src/client/js/otp/config.js
@@ -67,20 +67,38 @@ otp.config = {
      */
      
     baseLayers: [
-        {
-            name: 'MapQuest OSM',
-            tileUrl: 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
-            subdomains : ['otile1','otile2','otile3','otile4'],
-            attribution : 'Data, imagery and map information provided by <a href="http://open.mapquest.com" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.'
-        },
-        {
-            name: 'MapQuest Aerial',
-            tileUrl: 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png',
-            subdomains : ['otile1','otile2','otile3','otile4'],
-            attribution : 'Data, imagery and map information provided by <a href="http://open.mapquest.com" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.'
-        },           
+            {
+                name: 'Stamen Terrain',
+                tileUrl: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.png',
+                attribution : 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+            },
+            {
+                name: 'Carto Positron',
+                tileUrl: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+                attribution : 'Map tiles by Carto/MapZen. Map data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+            },
+            {
+                name: 'Transport Tiles',
+                tileUrl: 'http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png',
+                subdomains : ['a','b','c'],
+                attribution: 'Data from <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors. Tiles from <a href="http://www.thunderforest.com/transport/">Andy Allan</a>'
+            },
+            {
+                name: 'Stamen Toner Lite',
+                tileUrl: 'http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png',
+                attribution : 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+            },
+            {
+                name: 'Carto Dark Matter',
+                tileUrl: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
+                attribution : 'Map tiles by Carto/MapZen. Map data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+            },
+            {
+                name: 'OSM Standard Tiles',
+                tileUrl: 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                attribution : 'Map data and tiles © OpenStreetMap contributors'
+            }
     ],
-    
 
     /**
      * Map start location and zoom settings: by default, the client uses the
